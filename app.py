b7dd6d14e66a64822c47129e11d2021668c4b5f6
Karthikeya27 <70697178+Karthikeya27@users.noreply.github.com>
2023-09-14 06:12:18 +0000

    completed

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask,render_template
-from database import load_jobs_from_db
+from flask import Flask,render_template,jsonify,request
+from database import load_jobs_from_db,add_data_into_db,load_job_from_db
 app = Flask(__name__)
+
+
+
 
 
 @app.route("/")
 def homepage():
   job_list= load_jobs_from_db()
   return render_template("home.html", jobs = job_list)
+
+@app.route("/job/<id>")
+def job_info(id):
+  info = load_job_from_db(id)
+  return render_template("job_page.html",job=info)
+
+@app.route("/job/<id>/apply", methods = ["post"])
+
+def apply_to_jobs(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_data_into_db(id , data)
+  return render_template("application_submitted.html", data = data, job = job)
+  
+  
+  
+  
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0" , debug = True)
--- database.py
+++ database.py
@@ -1,10 +1,25 @@
 from sqlalchemy import create_engine,text
 import os
+from sqlalchemy.exc import SQLAlchemyError
+
 connection_string = os.environ["database_connection"]
 
 
 engine = create_engine(connection_string, echo=True)
+"""
+def test_data(person):
+  with engine.connect() as connection:
+    string = text("insert into Persons(PersonID, LastName, FirstName, Address, City) values(:PersonID, :LastName, :FirstName, :Address, :City)")
+    
+    connection.execute(statement= string,
+                     parameters=dict(PersonID = person["PersonID"],
+                      LastName = person["LastName"],
+                      FirstName = person["FirstName"],
+                      Address = person["Address"],
+                      City = person["City"]))
+"""
 
+                                      
 def load_jobs_from_db():
    with engine.connect() as connection:
     result = connection.execute(text("select * from jobs"))
@@ -14,11 +29,35 @@ def load_jobs_from_db():
     return(jobs)
 
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
 
 
-
-
-
+def add_data_into_db(job_id, application_details):
+    with engine.connect() as conn:
+        data = text("insert into application(id, name, email, education) values(:id, :name, :email, :education)")
+        params = {
+            "id": job_id,
+            "name": application_details['name'],
+            "email": application_details['email'],
+            "education": application_details['education']
+        }
+        conn.execute(data, params)
+        conn.commit()  # Ensure you commit the transaction.
+        
+  
+               
+              
+                                          
 
 
   
